3977c0dcbc68bf77f8b532fc4a62365b652ca275
Joanne Kam <joannekam@Joannes-MacBook-Pro.local>
2022-06-15 15:27:43 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Intro from './Components/Intro/intro';
+import About from './Components/about/about';
+import ProductList from './Components/productList/ProductList';
+import Contact from './Components/contact/Contact';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Intro />
+			<About />
+			<ProductList />
+			<Contact />
+		</div>
+	);
 }
 
 export default App;
