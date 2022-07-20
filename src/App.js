9f645edd1ff42819624115aa7ac77f1f35e426ef
Joanne Kam <joannekam@Joannes-MacBook-Pro.local>
2022-07-20 13:52:23 -0400

    revising

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
-import Intro from './Components/Intro/intro';
-import About from './Components/about/about';
-import ProductList from './Components/productList/ProductList';
-import Contact from './Components/contact/Contact';
 function App() {
-	return (
-		<div className='App'>
-			<Intro />
-			<About />
-			<ProductList />
-			<Contact />
-		</div>
-	);
+	return <div className='App'>home</div>;
 }
 
 export default App;
